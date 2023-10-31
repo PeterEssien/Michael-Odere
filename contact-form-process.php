0335865f1759283ba69e727b2e8330f6a7ed6994
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-10-31 13:24:01 +0100

    Update contact-form-process.php

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "essyenpeter57@gmail.com";
+    $email_subject = "New form submissions from website";
 
     function problem($error)
     {
